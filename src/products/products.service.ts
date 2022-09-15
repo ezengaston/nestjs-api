24867d8ce481ec619388d674fd13ba0551e01d42
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-15 21:14:32 +0300

    Products added and post request added

---
+++ src/products/products.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number): number {
+    const id = uuid();
+    const newProduct = new Product(id, title, desc, price);
+    this.products.push(newProduct);
+    return id;
+  }
+}
